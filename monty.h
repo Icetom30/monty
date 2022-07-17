d645550dd5989411dfc8050bb2c9421252e4ce45
Isaac Tom <101002204+Icetom30@users.noreply.github.com>
2022-07-18 00:02:22 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -47,7 +47,7 @@ typedef struct stack_s
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- * Authors: Martins Akhivbareme & Bolatito Alaba
+ * Authors: Isaac Tom
  *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
