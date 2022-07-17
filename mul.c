b17b80d4db9609cd2861fcba8dbb122a592291b5
Isaac Tom <101002204+Icetom30@users.noreply.github.com>
2022-07-17 23:52:13 +0100

    Update mul.c

--- mul.c
+++ mul.c
@@ -14,16 +14,16 @@
  */
 void _mul(stack_t **stack, unsigned int line_cnt)
 {
-  int result;
+	int result;
 
-  if (!stack || !*stack || !((*stack)->next))
-    {
-      fprintf(stderr, "L%d: can't mul, stack too short\n", line_cnt);
-      exit(EXIT_FAILURE);
-      return;
-    }
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_cnt);
+		exit(EXIT_FAILURE);
+		return;
+	}
 
-  result = ((*stack)->next->n) * ((*stack)->n);
-  pop(stack, line_cnt); /*For top node*/
-  (*stack)->n = result;
+	result = ((*stack)->next->n) * ((*stack)->n);
+	pop(stack, line_cnt); /*For top node*/
+	(*stack)->n = result;
 }
